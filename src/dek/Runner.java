7d644d1b9e0c06abb28df561e3bcebfa0bf6505b
Philip Dexter <philip.dexter@dektech.com.au>
2017-10-17 16:24:35 +0200

    Update state machine

--- src/dek/Runner.java
+++ src/dek/Runner.java
@@ -1,14 +1,23 @@
 package dek;
 
+import lejos.hardware.BrickFinder;
+import lejos.hardware.Button;
+import lejos.hardware.lcd.GraphicsLCD;
+
 public class Runner {
 	
+	GraphicsLCD LCD = BrickFinder.getDefault().getGraphicsLCD();
 	
 	State currentState = new Idle();
 	
 	public void go() {
 		while(true) {
 			currentState.run(this);
+			if (0 != Button.waitForAnyPress(5000)) {
+				break;
+			}
 		}
+		LCD.drawString("bye", 100, 20, GraphicsLCD.BASELINE|GraphicsLCD.HCENTER);
 	}
 	
 	public void setState(State state) {
