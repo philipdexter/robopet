126ed9d3080611703920cbd0215cf8da276177b1
Philip Dexter <philip.dexter@dektech.com.au>
2017-10-24 14:36:20 +0200

    Add simple wheel movement

--- src/dek/api/Wheels.java
+++ src/dek/api/Wheels.java
@@ -1,13 +1,40 @@
 package dek.api;
 
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
+
 public class Wheels {
+	
+	static RegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.B);
 
-	public void moveForward(int seconds) {
+	static RegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.C);
 
+	static {
+		leftMotor.setSpeed(400);
+		rightMotor.setSpeed(400);
+	}
+	
+	public static void setSpeed(int speed) {
+		leftMotor.setSpeed(speed);
+		rightMotor.setSpeed(speed);
+	}
+	
+	public static void moveForward(int seconds) {
+		leftMotor.forward();
+		rightMotor.forward();
+		Delay.msDelay(seconds);
+		leftMotor.stop();
+		rightMotor.stop();
 	}
 
-	public void moveBackward(int seconds) {
-
+	public static void moveBackward(int seconds) {
+		leftMotor.backward();
+		rightMotor.backward();
+		Delay.msDelay(seconds);
+		leftMotor.stop();
+		rightMotor.stop();
 	}
 
 	public void moveLeft(int seconds) {
