5909e1adc76cc4c831ae827d7eed4ab259cc5e53
Philip Dexter <philip.dexter@dektech.com.au>
2017-10-24 15:47:06 +0200

    Add claw api

--- src/dek/api/Claws.java
+++ src/dek/api/Claws.java
@@ -1,5 +1,26 @@
 package dek.api;
 
-public class Claws {
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
 
+public class Claws {
+	static RegulatedMotor motor = new EV3LargeRegulatedMotor(MotorPort.A);
+	
+	static {
+		motor.setSpeed(100);
+	}
+	
+	public static void open(int seconds) {
+		motor.backward();
+		Delay.msDelay(seconds*1000);
+		motor.stop();
+	}
+	
+	public static void close(int seconds) {
+		motor.forward();
+		Delay.msDelay(seconds*1000);
+		motor.stop();
+	}
 }
