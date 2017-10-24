130d3086b3661d49ef50a93f2440d5d895f5ea43
farhan.khan <farhan.khan@172.19.5.143>
2017-10-24 14:52:26 +0200

    i added a very important exclimation mark !!

--- src/dek/Idle.java
+++ src/dek/Idle.java
@@ -4,7 +4,7 @@ import dek.api.Screen;
 
 public class Idle implements State {
 	public boolean run(Runner runner) {
-		Screen.print_centered("Current state is Idle", 100, 20);
+		Screen.print_centered("Current state is Idle!", 100, 20); 
 		
 		return false;
 	}
