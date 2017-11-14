e0c3ed789824a40ec98a60515afdd0f61dad3ab1
Philip Dexter <philip.dexter@dektech.com.au>
2017-11-14 14:21:48 +0100

    Rename philip to colorfinder

--- src/dek/ColorFinder.java
+++ src/dek/ColorFinder.java
@@ -4,8 +4,9 @@ import dek.api.Screen;
 import dek.api.Wheels;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.Color;
 
-public class Philip implements State {
+public class ColorFinder implements State {
 	
 	/*
 	 * go forward until we find yellow
@@ -19,11 +20,15 @@ public class Philip implements State {
 		EV3ColorSensor sensor = new EV3ColorSensor(SensorPort.S4);
 		Wheels.setSpeed(100);
 		Wheels.go();
+		int last_color = 0;
 		while (true) {
 			int id = sensor.getColorID();
-			Screen.print_centered("Color " + id, 100, 20);
-			
-			if (id == 3) break;
+			if (last_color != id) {
+				Screen.clear();
+				Screen.print_centered("Color " + id, 100, 20);
+			}
+			last_color = id;
+			if (id == Color.BLUE) break;
 		}
 		Wheels.stop();
 		
