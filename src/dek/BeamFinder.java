8e0919cd3bfb89bb1805f783e4f5421df32c61cd
mai.phuong.van <mai.phuong.van@172.19.5.178>
2017-11-28 16:23:44 +0100

    Getting seek mode

--- src/dek/BeamFinder.java
+++ src/dek/BeamFinder.java
@@ -16,12 +16,26 @@ public class BeamFinder {
 		
 		while(true) {
 		EV3IRSensor sensor = new EV3IRSensor(SensorPort.S3);
+		sensor.setCurrentMode(1);
+		
+		int mode = sensor.getCurrentMode();
+		SensorMode sensorMode = sensor.getMode(mode);
+		
+		Screen.print_centered("Current mode " + sensorMode.getName() , 90, 20);
+		
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		retValueIR=sensor.getSeekMode();
 		
 		sampleSize= retValueIR.sampleSize();
-		
 		float[] sample= new float[sampleSize];
+		
+		
 		
 		followTheBeacon(sample);
 		
@@ -41,7 +55,14 @@ public class BeamFinder {
 		
 		retValueIR.fetchSample(sample, 0);
 				
-		Screen.print_centered("Ist " + (int)sample[0]*100 + " 2nd" + (int)sample[1] *100, 100, 20);
+		Screen.print_centered("1st " + (int)sample[0]*100 + 
+				" 2nd " + (int)sample[1] *100, 100, 50);
+		Screen.print_centered("3rd " + (int)sample[2]*100 + 
+				" 4th " + (int)sample[3] *100, 100, 70);
+		Screen.print_centered("5th " + (int)sample[4]*100 + 
+				" 6th " + (int)sample[5] *100, 100, 90);
+		Screen.print_centered("7th " + (int)sample[6]*100 + 
+				" 8th " + (int)sample[7] *100, 100, 110);
 		
 		Button.waitForAnyPress();
 		
